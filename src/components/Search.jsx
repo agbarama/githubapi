29796340684ddf670c9938bc2ceaf2cb03c4baa9
agbarama <agbaramafavour4@gmail.com>
2024-03-25 19:39:16 +0100

    Created and styled the search component.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import styles from "../styles/search.module.css";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [input, setInput] = useState("");
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.input}
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.currentTarget.value)}
+      />
+    </div>
+  );
 };
 
 export default Search;
