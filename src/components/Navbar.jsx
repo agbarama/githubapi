4ce9cb25e12944ce60c60c52334b958f51ca81e5
agbarama <agbaramafavour4@gmail.com>
2024-03-25 19:21:15 +0100

    Created work space

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/navbar.module.css";
+const Navbar = () => {
+  return (
+    <div className={styles.nav}>
+      <link rel="stylesheet" href="../" />
+      <h2 className={styles.header}>Github</h2>
+    </div>
+  );
+};
+
+export default Navbar;
