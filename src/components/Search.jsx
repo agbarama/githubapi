88f7b189843b0fbfdc38244f2dcdcecc53078ebf
agbarama <agbaramafavour4@gmail.com>
2024-04-02 20:59:25 +0100

    Added and styled radio input.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -22,16 +22,34 @@ const Search = ({ result, setResult, page, setPage }) => {
   }, [page]);
 
   return (
-    <div className={styles.container}>
-      <input
-        className={styles.input}
-        type="text"
-        placeholder="search for users and organization"
-        value={input}
-        onChange={(e) => setInput(e.currentTarget.value)}
-      />
-      <div className={styles.search} onClick={() => setPage(page + 1)}>
-        search
+    <div>
+      <div className={styles.container}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="search for users and organization"
+          value={input}
+          onChange={(e) => setInput(e.currentTarget.value)}
+        />
+        <div className={styles.search} onClick={() => setPage(page + 1)}>
+          search
+        </div>
+      </div>
+      <div className={styles.radio}>
+        <input className={styles.input} type="radio" name="user" id="user" />
+        <label className={styles.label} htmlFor="">
+          User
+        </label>
+
+        <input
+          className={styles.input}
+          type="radio"
+          name="organization"
+          id="organization"
+        />
+        <label className={styles.label} htmlFor="oragnization">
+          Oragnization
+        </label>
       </div>
     </div>
   );
